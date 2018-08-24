bb03aacc7b4365e6ce0d2eb6d703f45809a583d7
Alex Rusnak <rusalem@gmail.com>
2018-08-23 23:38:22 -0700

    cp

--- projects/TensorFlow/HelloTF.py
+++ projects/TensorFlow/HelloTF.py
@@ -1,5 +1,16 @@
+import numpy as np
 import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!')
+a = tf.constant(3.0, dtype=tf.float32)
+b = tf.constant(4.0) # also tf.float32 implicitly
+total = a + b
+print(a)
+print(b)
+print(total)
 sess = tf.Session()
-print(sess.run(hello))
+result = sess.run((total, hello))
+print(result)
+
+writer = tf.summary.FileWriter('logs')
+writer.add_graph(tf.get_default_graph())
